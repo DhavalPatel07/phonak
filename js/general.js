bb1be05deeac6e08b0d447a0ef0d57edffa52f6d
Dhaval Patel <30649833+DhavalPatel07@users.noreply.github.com>
2022-06-17 09:16:20 +0530

    Changes
    Added animation and logo

--- js/general.js
+++ js/general.js
@@ -13,7 +13,11 @@ $(document).ready(function(){
         }
         else {window.location.href = target_name; }
     });   
-    $(".top-btn").on("click", function(){
-    	$(".firstfield input").focus();
-    })
+    $(".top-btn").on("click", function(){$(".firstfield input").focus(); });
+
+    // Animation
+    (function($) {
+      $.fn.visible = function(partial) {var $t = $(this), $w = $(window), viewTop = $w.scrollTop(), viewBottom = viewTop + $w.height(), _top = $t.offset().top, _bottom = _top + $t.height(), compareTop = partial === true ? _bottom : _top, compareBottom = partial === true ? _top : _bottom; return ((compareBottom <= viewBottom) && (compareTop >= viewTop)); }; })(jQuery); var win = $(window); var allMods = $(".animation-effect"); allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {el.addClass("come-in"); }; }); win.scroll(function(event) {allMods.each(function(i, el) {var el = $(el); if (el.visible(true)) {setTimeout(function(){el.addClass("come-in"); }, 600); }; /*else {el.removeClass("come-in"); } */ });
+    });
+
 });
